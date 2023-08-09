11992425d3602537023218fe8d56d9f9805230cf
Sebastian Ocando <ocandovivas@gmail.com>
2023-08-09 15:32:05 -0300

    button hover

--- src/global/components/Nav/Button.jsx
+++ src/global/components/Nav/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = (props) => {
     return (
-        <button className='flex w-[220px] border border-gray-300 rounded-md text-white py-2 px-6 rounded lg:ml-8 bg-gray-800 hover:bg-lightGray/[.09]
+        <button className='flex w-[220px] border border-gray-300 rounded-md text-white py-2 px-6 rounded lg:ml-8 bg-gray-800 lg:hover:bg-lightGray/[.09]
         duration-300'>
             {props.children}
         </button>
