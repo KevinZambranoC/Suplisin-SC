6ab7170363ba3f04723961d391f8a4cf9f1b64db
Sebastian Ocando <ocandovivas@gmail.com>
2023-08-11 21:01:41 -0300

    output option

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve('./src/components/Home/Carousel.jsx'),
+      name: 'react-icons',
+      filename: '"react-icons": "^4.10.1",'
+    },
+    rollupOptions: {
+      external: ['react', 'react-icons": "^4.10.1'],
+      output: {
+        globals: {
+          react: 'react'
+        }
+      }
+    }
+  }
 })
