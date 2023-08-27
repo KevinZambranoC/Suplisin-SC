f040c0d947b224d641968062c0a4c1a41e844ee5
Sebastian Ocando <ocandovivas@gmail.com>
2023-08-26 22:52:36 -0300

    img size

--- src/components/Home/Texture.jsx
+++ src/components/Home/Texture.jsx
@@ -10,24 +10,24 @@ export const Texture = () => {
                 <p className=" max-w-2xl text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed m-auto mt-6">If you're looking to add some personality and visual interest to your home's interior, textures are a great way to achieve that. From smooth and sleek to rough and rustic, there are plenty of popular types of texture to choose from when decorating your home.</p>
                 <div className="p-4 mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-4 ">
                     <div className="block bg-white/[.85] rounded-lg shadow-xl text-center">
-                        <img alt="drywall" src="https://i.ibb.co/TTqdFXz/finish-1.webp" className="h-64 rounded-t-lg w-full object-cover sm:h-80 lg:h-96"
+                        <img alt="drywall" src="https://i.ibb.co/6v15zMm/finish-1-1.webp" className="h-64 rounded-t-lg w-full object-cover sm:h-80 lg:h-96"
                         />
                         <h3 className="mt-4 text-lg font-semibold text-gray-900 sm:text-2xl ">Drywall </h3>
                         <p className="mt-2 m-auto max-w-sm text-gray-700">Drywall Installation</p>
                     </div>
                     <div className="block bg-white/[.85] rounded-lg shadow-xl">
-                        <img alt="drywall_finish" src="https://i.ibb.co/CnjDDPk/finish-5.webp" className="h-64 w-full rounded-t-lg  object-cover sm:h-80 lg:h-96"/>
+                        <img alt="drywall_finish" src="https://i.ibb.co/BB3w6c8/finish-5-1.webp" className="h-64 w-full rounded-t-lg  object-cover sm:h-80 lg:h-96"/>
                         <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">Finishing</h3>
                         <p className="mt-2 m-auto  max-w-sm text-gray-700">Finish Drywall</p>
                     </div>
 
                     <div className="block bg-white/[.85] rounded-lg shadow-xl">
-                        <img alt="texture" src="https://i.ibb.co/m5d7tkk/knockdowm.webp" className="h-64 w-full rounded-t-lg  object-cover sm:h-80 lg:h-96"/>
+                        <img alt="texture" src="https://i.ibb.co/gvqM4f7/knockdowm-1.webp" className="h-64 w-full rounded-t-lg  object-cover sm:h-80 lg:h-96"/>
                         <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">Texture </h3>
                         <p className="mt-2 m-auto  max-w-sm text-gray-700">Orange Peel</p>
                     </div>
                     <div className="block bg-white/[.85] rounded-lg shadow-xl">
-                        <img alt="texture" src="https://i.ibb.co/pXVQd1P/texure.webp" className="h-64 w-full rounded-t-lg  object-cover sm:h-80 lg:h-96"/>
+                        <img alt="texture" src="https://i.ibb.co/ysy4CXz/texure-1.webp" className="h-64 w-full rounded-t-lg  object-cover sm:h-80 lg:h-96"/>
                         <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">Texture </h3>
                         <p className="mt-2 m-auto  max-w-sm text-gray-700">Knockdowm Texturte
                         </p>
