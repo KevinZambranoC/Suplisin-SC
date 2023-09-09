b85beab1a3928dfcbacda8e8a6061b44edfa6c55
Sebastian Ocando <ocandovivas@gmail.com>
2023-09-09 18:53:32 -0300

    mobile menu z-index

--- src/components/Projects/Phrase.jsx
+++ src/components/Projects/Phrase.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Phrase = () => {
     return (
         <div className='relative h-[460px] md:h-[360px]'>
-            <div className="bg-cover bg-center brightness-75 h-full object-fill bg-[url(https://images.unsplash.com/photo-1607400201515-c2c41c07d307?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)]">
+            <div className="bg-cover bg-center brightness-50 h-full object-fill bg-[url(https://images.unsplash.com/photo-1607400201515-c2c41c07d307?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)]">
             </div>
             <div className="md:w-1/2 absolute top-0 text-white  py-24 px-10">
                     <p className="font-bold text-sm uppercase">Services</p>
