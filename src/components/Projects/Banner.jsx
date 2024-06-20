867a49d6631c8766002aa84894317feb58294676
kevin <kevinzambranocastro@gmail.com>
2024-06-20 07:16:41 -0400

    Ultimos cambios realizados

--- src/components/Projects/Banner.jsx
+++ src/components/Projects/Banner.jsx
@@ -5,12 +5,12 @@ export const Banner = () => {
         <div className="mt-20 shadow-xl">
             <div className="relative">
                 <img
-                    src="https://images.unsplash.com/photo-1613621792067-8e28d16b735c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                    src="https://i.ibb.co/1sBX07X/Captura-de-pantalla-808.png"
                     className="w-full object-cover h-[340px] md:h-[420px] block"/>
                 <div className="w-full h-full bg-black opacity-50 absolute top-0 left-0" />
                 <div className="absolute top-1/4 mt-12 w-full text-center text-white font-semibold ">
-                    <p className='tracking-wide mb-4'>HOME • PROJECTS</p>
-                    <h2 className="lg:text-7xl md:tex-4xl text-3xl leading-9 ">Our Projects</h2>
+                    <p className='tracking-wide mb-4'>Inicio • Productos</p>
+                    <h2 className="lg:text-7xl md:tex-4xl text-3xl leading-9 ">Nuestros Productos</h2>
                 </div>
             </div>
         </div>
--- src/components/Projects/Phrase.jsx
+++ src/components/Projects/Phrase.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 export const Phrase = () => {
     return (
         <div className='relative h-[460px] md:h-[360px]'>
-            <div className="bg-cover bg-center brightness-50 h-full object-fill bg-[url(https://images.unsplash.com/photo-1607400201515-c2c41c07d307?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)]">
+            <div className="bg-cover bg-center brightness-50 h-full object-fill bg-[url(https://i.ibb.co/ZLG8pRH/Captura-de-pantalla-809.png)]">
             </div>
             <div className="md:w-1/2 absolute top-0 text-white  py-24 px-10">
-                    <p className="font-bold text-sm uppercase">Services</p>
-                    <p className="text-3xl font-bold">Flawless Wall Finishes You Can Trust</p>
-                    <p className="text-2xl mb-10 leading-none text-offWhite">Transforming Spaces with Reliable Wall Finishes: Our Commitment to Excellence</p>
-                    <Link to='/contact' className="bg-purple-800 py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800">Contact us</Link>
+                    <p className="font-bold text-sm uppercase">Servicios</p>
+                    <p className="text-3xl font-bold">Acabados de Calzado Industrial en los que puedes confiar</p>
+                    <p className="text-2xl mb-10 leading-none text-offWhite">Transformando espacios con calzado industrial confiable: Nuestro compromiso con la excelencia</p>
+                    <Link to='/contact' className="bg-purple-800 py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800">Contáctanos</Link>
                 </div>  
         </div>
     )
